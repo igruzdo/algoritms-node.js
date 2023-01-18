8247999f50a8d39b3f08f5177bf551cc0bf1c11d
Igor Gruzdo <igruzdo@yandex.ru>
2023-01-18 21:40:49 +0300

    finished final_6 task 1, 2

--- sprint_6/final/task_1.js
+++ sprint_6/final/task_1.js
@@ -58,88 +58,93 @@ Oops! I did it again
     Посылка немного отличается в нейминге меодов, так как я для понимания внес более понятные имена, в остальном всё 100% идентично. 
 */
 class PriorityQueue {
-    constructor() {
-      this.heap = [];
+  constructor() {
+    this.heap = [];
+  }
+  /**
+   * @param {[string, number]} edge - ребро с концом и весом
+   * @returns {void}
+   */
+  enqueue(edge) {
+    this.heap.push(edge);
+    this._siftUp();
+  }
+  /**
+   * @returns {[string, number]} - ребро с концом и весом
+   */
+  dequeue() {
+    const max = this.heap[0];
+    const last = this.heap.pop();
+    if (this.heap.length > 0) {
+      this.heap[0] = last;
+      this._siftDown();
     }
-    /**
-    * @param {[string, number]} edge - ребро с концом и весом
-    * @returns {void}
-    */
-    enqueue(edge) {
-      this.heap.push(edge);
-      this._siftUp();
-    }
-    /**
-    * @returns {[string, number]} - ребро с концом и весом
-    */
-    dequeue() {
-      const max = this.heap[0];
-      const last = this.heap.pop();
-      if (this.heap.length > 0) {
-        this.heap[0] = last;
-        this._siftDown();
-      }
-      return max;
-    }
-    /**
-    * @returns {void}
-    */
-    _siftUp() {
-      let index = this.heap.length - 1;
+    return max;
+  }
+  /**
+   * @returns {void}
+   */
+  _siftUp() {
+    let index = this.heap.length - 1;
 
-      while (index > 0) {
-        let parentIndex = Math.floor((index - 1) / 2);
-        if (this.heap[index][1] <= this.heap[parentIndex][1]) break;
-        [this.heap[index], this.heap[parentIndex]] = [this.heap[parentIndex], this.heap[index]];
-        index = parentIndex;
-      }
-    }
-    /**
-    * @returns {void}
-    */
-    _siftDown() {
-      let index = 0;
-      while (true) {
-        let leftChildIndex = 2 * index + 1;
-        let rightChildIndex = 2 * index + 2;
-        let leftChild, rightChild;
-        let swapIndex = null;
-
-        if (leftChildIndex < this.heap.length) {
-          leftChild = this.heap[leftChildIndex];
-          if (leftChild[1] > this.heap[index][1]) {
-            swapIndex = leftChildIndex;
-          }
-        }
-
-        if (rightChildIndex < this.heap.length) {
-          rightChild = this.heap[rightChildIndex];
-          if (
-            (swapIndex === null && rightChild[1] > this.heap[index][1]) ||
-            (swapIndex !== null && rightChild[1] > leftChild[1])
-          ) {
-            swapIndex = rightChildIndex;
-          }
-        }
-
-        if (swapIndex === null) break;
-        [this.heap[index], this.heap[swapIndex]] = [this.heap[swapIndex], this.heap[index]];
-        index = swapIndex;
-      }
-    }
-    /**
-    * @returns {number}
-    */
-    size() {
-        return this.heap.length;
+    while (index > 0) {
+      let parentIndex = Math.floor((index - 1) / 2);
+      if (this.heap[index][1] <= this.heap[parentIndex][1]) break;
+      [this.heap[index], this.heap[parentIndex]] = [
+        this.heap[parentIndex],
+        this.heap[index],
+      ];
+      index = parentIndex;
     }
   }
-  
+  /**
+   * @returns {void}
+   */
+  _siftDown() {
+    let index = 0;
+    while (true) {
+      let leftChildIndex = 2 * index + 1;
+      let rightChildIndex = 2 * index + 2;
+      let leftChild, rightChild;
+      let swapIndex = null;
 
-const readline = require('readline');
+      if (leftChildIndex < this.heap.length) {
+        leftChild = this.heap[leftChildIndex];
+        if (leftChild[1] > this.heap[index][1]) {
+          swapIndex = leftChildIndex;
+        }
+      }
+
+      if (rightChildIndex < this.heap.length) {
+        rightChild = this.heap[rightChildIndex];
+        if (
+          (swapIndex === null && rightChild[1] > this.heap[index][1]) ||
+          (swapIndex !== null && rightChild[1] > leftChild[1])
+        ) {
+          swapIndex = rightChildIndex;
+        }
+      }
+
+      if (swapIndex === null) break;
+      [this.heap[index], this.heap[swapIndex]] = [
+        this.heap[swapIndex],
+        this.heap[index],
+      ];
+      index = swapIndex;
+    }
+  }
+  /**
+   * @returns {number}
+   */
+  size() {
+    return this.heap.length;
+  }
+}
+
+const readline = require("readline");
 
 const rl = readline.createInterface({
-    input: process.stdin,
+  input: process.stdin,
 });
 
 let listNeighbors = {};
@@ -149,112 +154,107 @@ let numberV;
 let numberE;
 let notAdded = new Set();
 let edges = new PriorityQueue();
-const errorMessage = 'Oops! I did it again';
+const errorMessage = "Oops! I did it again";
 
-rl.on('line', (input) => {
-    if(phase === 0) {
-        numberV = +input.split(' ')[0]
-        numberE = +input.split(' ')[1]
-        for(let i = 1; i <= numberV; i++) {
-            notAdded.add(i);
+rl.on("line", (input) => {
+  if (phase === 0) {
+    numberV = +input.split(" ")[0];
+    numberE = +input.split(" ")[1];
+    for (let i = 1; i <= numberV; i++) {
+      notAdded.add(i);
+    }
+  }
+  if (phase > 0 && phase <= numberE) {
+    const parsedInputArray = input.split(" ");
+    const parsedStart = +parsedInputArray[0];
+    const parsedEnd = +parsedInputArray[1];
+    const parsedUnputWeight = +parsedInputArray[2];
+
+    if (parsedStart !== parsedEnd) {
+      if (listNeighbors[parsedStart]) {
+        const graph = listNeighbors[parsedStart];
+        const weight = graph[parsedEnd];
+
+        if (weight >= 0) {
+          graph[parsedEnd] =
+            weight < parsedUnputWeight ? parsedUnputWeight : weight;
+        } else {
+          graph[parsedEnd] = parsedUnputWeight;
         }
-    }
-    if(phase > 0 && phase <= numberE) {
-        const parsedInputArray = input.split(' ');
-        const parsedStart = +parsedInputArray[0];
-        const parsedEnd = +parsedInputArray[1];
-        const parsedUnputWeight = +parsedInputArray[2];
+      } else {
+        listNeighbors[parsedStart] = {
+          [parsedEnd]: parsedUnputWeight,
+        };
+      }
 
-        if(parsedStart !== parsedEnd) {
-            if(listNeighbors[parsedStart]) {
-                const graph = listNeighbors[parsedStart];
-                const weight = graph[parsedEnd];
+      if (listNeighbors[parsedEnd]) {
+        const graph = listNeighbors[parsedEnd];
+        const weight = graph[parsedStart];
 
-                if(weight >=0) {
-                    graph[parsedEnd] = weight < parsedUnputWeight ? parsedUnputWeight : weight;
-                } else {
-                    graph[parsedEnd] = parsedUnputWeight;
-                }
-            } else {
-                listNeighbors[parsedStart] = {
-                    [parsedEnd]: parsedUnputWeight,
-                }
-            }
-    
-            if(listNeighbors[parsedEnd]) {
-                const graph = listNeighbors[parsedEnd];
-                const weight = graph[parsedStart];
-
-                if(weight >=0) {
-                    graph[parsedStart] = weight < parsedUnputWeight ? parsedUnputWeight : weight;
-                } else {
-                    graph[parsedStart] = parsedUnputWeight;
-                }
-
-            } else {
-                listNeighbors[parsedEnd] = {
-                    [parsedStart]: parsedUnputWeight,
-                }
-            }
+        if (weight >= 0) {
+          graph[parsedStart] =
+            weight < parsedUnputWeight ? parsedUnputWeight : weight;
+        } else {
+          graph[parsedStart] = parsedUnputWeight;
         }
+      } else {
+        listNeighbors[parsedEnd] = {
+          [parsedStart]: parsedUnputWeight,
+        };
+      }
     }
+  }
 
-    if(phase >= numberE) {
-        rl.emit('close');
-    }
-    phase++
+  if (phase >= numberE) {
+    rl.emit("close");
+  }
+  phase++;
 });
 
-rl.on('close', () => {
-    process.stdout.write(`${findMST()}`);
-})
-
+rl.on("close", () => {
+  process.stdout.write(`${findMST()}`);
+});
 
 /**
-* @param {string} v - вершина графа
-* @returns {void}
-*/
+ * @param {string} v - вершина графа
+ * @returns {void}
+ */
 function addVertex(v) {
-    notAdded.delete(+v);
+  notAdded.delete(+v);
 
-    if(listNeighbors[v]) {
-        let graph = listNeighbors[v];
+  if (listNeighbors[v]) {
+    let graph = listNeighbors[v];
 
-        for(let key in graph) {
-            if(notAdded.has(Number(key))) {
-                edges.enqueue([key, graph[key]]);
-            }
-        }
+    for (let key in graph) {
+      if (notAdded.has(Number(key))) {
+        edges.enqueue([key, graph[key]]);
+      }
     }
+  }
 }
 
 /**
-* @returns {number | `${errorMessage}`}
-*/
+ * @returns {number | `${errorMessage}`}
+ */
 
 function findMST() {
-    let maximumSpanningWeight = 0;
-    let v = 1;
+  let maximumSpanningWeight = 0;
+  let v = 1;
 
-    addVertex(v);
+  addVertex(v);
 
-    while(notAdded.size > 0 && edges.size() > 0) {
-        let e = edges.dequeue();
+  while (notAdded.size > 0 && edges.size() > 0) {
+    let e = edges.dequeue();
 
-        if(notAdded.has(+e[0])) {
-            maximumSpanningWeight += e[1];
-            addVertex(e[0]);
-        }
+    if (notAdded.has(+e[0])) {
+      maximumSpanningWeight += e[1];
+      addVertex(e[0]);
     }
+  }
 
-    if(notAdded.size > 0) {
-        return errorMessage;
-    } else {
-        return maximumSpanningWeight;
-    }
+  if (notAdded.size > 0) {
+    return errorMessage;
+  } else {
+    return maximumSpanningWeight;
+  }
 }
-
-
-
-
-
